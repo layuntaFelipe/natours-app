adb6c3d50c7a500c4524f17a50572c1fb93859dc
layuntaFelipe <layunta.felipe@gmail.com>
2022-08-15 15:42:49 -0300

    staring-server-file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server....');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello from the server side!',
+    app: 'Natours'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`app running on port: ${PORT}`);
+});
